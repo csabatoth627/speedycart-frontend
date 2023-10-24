b66c1da8b3061625dda92067606d267e696d4ce2
Csaba Toth <csabi627@gmail.com>
2023-10-24 21:27:08 +0200

    feat/create rating component

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa"
+
+const Rating = ({value, text}) => {
+  return (
+    <div className='rating'>
+        <span>
+            {value >=1 ? <FaStar/> : value >= 0.5 ? <FaStarHalfAlt/> :
+            <FaRegStar/>}
+        </span>
+    </div>
+  )
+}
+
+export default Rating
