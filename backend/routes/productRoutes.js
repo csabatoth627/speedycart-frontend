4fe63470c5b4b7dc7f4d48b63d159ba89d437542
Csaba Toth <csabi627@gmail.com>
2023-10-26 23:32:09 +0200

    feat/creat async handler function

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,15 @@
 import express from "express"
 const router = express.Router();
 import products from "../data/products.js";
+import asyncHandler from "../middleware/asyncHandler.js";
 
-router.get('/', (req, res) => {
+router.get('/', asyncHandler( async(req, res) => {
     res.json(products)
-});
+}));
 
-router.get('/:id', (req, res) => {
+router.get('/:id', asyncHandler( async(req, res) => {
     const product = products.find((p) => p._id === req.params.id);
     res.json(product);
-});
+}));
 
 export default router;
