42fb0136dff55d90bd7ee5c1b5dce09b5bd88df8
Csaba Toth <csabi627@gmail.com>
2023-10-27 19:58:17 +0200

    feat/(server.js) import errorHandler and notFound

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import { log } from 'console';
-import dotenv from 'dotenv';
+import { log } from "console";
+import dotenv from "dotenv";
 dotenv.config();
-import connectDb from './config/db.js';
-import express from 'express';
-import productRoutes from './routes/productRoutes.js'
+import connectDb from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import express from "express";
+import productRoutes from "./routes/productRoutes.js";
 
 connectDb();
 
@@ -11,10 +12,13 @@ const port = process.env.PORT;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running....')
+app.get("/", (req, res) => {
+  res.send("API is running....");
 });
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
