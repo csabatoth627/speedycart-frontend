e3ab573e879a2678c79397308a96dec4037db85f
Csaba <csabi627@gmail.com>
2023-11-28 19:26:05 +0100

    feat:initial cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDb from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js"
 
@@ -16,6 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("API is running....");
