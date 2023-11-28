0f62f86508db36759e5ad7a6c1f782b777b2edd5
Csaba <csabi627@gmail.com>
2023-11-28 22:10:37 +0100

    feat:create registerUser(userController.js)

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -23,7 +23,34 @@ const authUser = asyncHandler(async (req, res) => {
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.send('Register user')
+    const {email, name, password} = req.body;
+
+    const userExist = await User.findOne({email});
+
+    if (userExist){
+        res.status(400)
+        throw new Error('User already exist');
+    };
+
+    const user = await User.create({
+        email,
+        name,
+        password,
+    });
+
+    if(user){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
