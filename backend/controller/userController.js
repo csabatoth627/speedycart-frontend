7af9b8a2c6cddbc676c1e0dd87d0c140a04c9a93
Csaba <csabi627@gmail.com>
2023-11-28 17:15:27 +0100

    feat:create userController.js

---
+++ backend/controller/userController.js
@@ -0,0 +1,34 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel"
+
+const authUser = asyncHandler(async (req, res) => {
+    res.send('Authenticate user')
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+    res.send('Register user')
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+    res.send('Logout user')
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.send('Get user profile')
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.send('Update user profile')
+});
+
+const getUsers = asyncHandler(async (req, res) => {
+    res.send('Get users')
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+    res.send('Delete user')
+});
+
+const getUserByID = asyncHandler(async (req, res) => {
+    res.send('Get user by id')
+});
