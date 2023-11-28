7c234383395946cadb13a20465298a3448db1566
Csaba <csabi627@gmail.com>
2023-11-28 11:53:00 +0100

    feat:add remove button (CartScreen.jsx)

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -12,6 +12,7 @@ import {
   ListGroupItem,
 } from "react-bootstrap";
 import Message from "../components/Message";
+import { FaTrash } from "react-icons/fa";
 
 const CartScreen = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -20,7 +21,7 @@ const CartScreen = () => {
   return (
     <Row>
       <Col md={8}>
-        <h1 style={{marginBottom:'20px'}}>Shopping Cart</h1>
+        <h1 style={{ marginBottom: "20px" }}>Shopping Cart</h1>
         {cartItems.length === 0 ? (
           <Message>
             Your cart is empty! <Link to="/">Go Back</Link>
@@ -28,34 +29,35 @@ const CartScreen = () => {
         ) : (
           <ListGroup variant="flush">
             {cartItems.map((item) => (
-                <ListGroupItem key={item._id}>
-                    <Row>
-                        <Col md={2}>
-                            <Image src={item.image} alt={item.name} fluid rounded/>
-                        </Col>
-                        <Col md={4}>
-                        <Link to={`/product/${item._id}`}>{item.name}</Link>
-                        </Col>
-                        <Col md={2}>
-                            ${item.price}
-                        </Col>
-                        <Col md={2}>
-                        <Form.Control
-                            as="select"
-                            value={item.qty}
-                            onChange={(e) => {}}
-                          >
-                            {[...Array(item.countInStock).keys()].map(
-                              (num) => (
-                                <option key={num + 1} value={num + 1}>
-                                  {num + 1}
-                                </option>
-                              )
-                            )}
-                          </Form.Control>
-                        </Col>
-                    </Row>
-                </ListGroupItem>
+              <ListGroupItem key={item._id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={4}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) => {}}
+                    >
+                      {[...Array(item.countInStock).keys()].map((num) => (
+                        <option key={num + 1} value={num + 1}>
+                          {num + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button type="button" variant="danger">
+                      <FaTrash />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroupItem>
             ))}
           </ListGroup>
         )}
