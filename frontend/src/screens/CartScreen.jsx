468af2d041c992e144d770655c2b176dc009836a
Csaba <csabi627@gmail.com>
2023-11-28 12:42:39 +0100

    feat:change quantity is available (CartScreen.jsx)

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { addItemToCart } from "../slices/cartSlice";
 import {
   ListGroup,
   Row,
@@ -17,6 +18,10 @@ import { FaTrash } from "react-icons/fa";
 const CartScreen = () => {
   const { cartItems } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+
+  const addToCartHandler = async (product, qty) => {
+    dispatch(addItemToCart({ ...product, qty }));
+  };
 
   return (
     <Row>
@@ -37,12 +42,19 @@ const CartScreen = () => {
                   <Col md={4}>
                     <Link to={`/product/${item._id}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    }).format(item.price * item.qty)} 
+                  </Col>
                   <Col md={2}>
                     <Form.Control
                       as="select"
                       value={item.qty}
-                      onChange={(e) => {}}
+                      onChange={(e) => {
+                        addToCartHandler(item, Number(e.target.value));
+                      }}
                     >
                       {[...Array(item.countInStock).keys()].map((num) => (
                         <option key={num + 1} value={num + 1}>
@@ -70,12 +82,16 @@ const CartScreen = () => {
                 Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                 items
               </h2>
-              $ ({cartItems.reduce((acc,item) => acc + item.qty * item.price,0).toFixed(2)})
+              $ (
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+              )
             </ListGroupItem>
             <ListGroupItem>
-                <Button type="button" variant="primary">
-                        Proceed To Checkout
-                </Button>
+              <Button type="button" variant="primary">
+                Proceed To Checkout
+              </Button>
             </ListGroupItem>
           </ListGroup>
         </Card>
