bc1f3b5bf7364047a2ea65f1724691a46e7e9d1b
Csaba <csabi627@gmail.com>
2023-11-29 10:26:02 +0100

    feat:complete loginScreen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Form, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
 
 const LoginScreen = () => {
-  return (
-    <div>Hello from Login Screen..</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LoginScreen
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <FormContainer>
+      <Form>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </Form.Group>
+        <Button type="button" variant="primary" className="mt-2">
+          Sign In
+        </Button>
+      </Form>
+      <Row className="py-3">
+        <Col>
+        New Customer? <Link to='/register'>Start here.</Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
