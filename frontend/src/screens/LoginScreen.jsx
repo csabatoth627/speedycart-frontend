6ab6bdbd411920dae23b0830826b470fe646c3bd
Csaba <csabi627@gmail.com>
2023-11-29 09:25:55 +0100

    feat:create loginScreen.jsx

---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginScreen = () => {
+  return (
+    <div>Hello from Login Screen..</div>
+  )
+}
+
+export default LoginScreen
---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,16 @@
+import { apiSlice } from "./apiSlice";
+import { USERS_URL } from "../constants";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+      login: builder.mutation({
+        query: (data) => ({
+          url: `${USERS_URL}/register`,
+          method: 'POST',
+          body: data,
+        }),
+      })
+    })
+  });
+
+  export const {useLoginMutation} =  userApiSlice;
