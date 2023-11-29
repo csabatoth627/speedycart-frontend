fda3b30601ae3f8f20e5469b8a794c3c5da5eaf5
Csaba <csabi627@gmail.com>
2023-11-29 20:54:23 +0100

    feat:create checkot components

--- src/screens/ShippingScreen.jsx
+++ src/screens/ShippingScreen.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "../slices/cartSlice";
 import FormContainer from "../components/FormContainer";
 import { Form, Button } from "react-bootstrap";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
   const { shippingAdress } = useSelector((state) => state.cart);
@@ -24,6 +25,7 @@ const ShippingScreen = () => {
   };
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2/>
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="formBasicAddress">
