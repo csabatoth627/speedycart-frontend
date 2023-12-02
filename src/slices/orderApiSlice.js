bd01ae188fa50e56d4d9535df70303ffb25a6000
Csaba <csabi627@gmail.com>
2023-12-02 14:11:41 +0100

    feat:create payOrder and getPayPalClient apislice(orderApiSlice.js)

--- src/slices/orderApiSlice.js
+++ src/slices/orderApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from "./apiSlice";
-import { ORDERS_URL } from "../constants";
+import { ORDERS_URL, PAYPAL_URL } from "../constants";
 
 export const orderApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
@@ -14,6 +14,17 @@ export const orderApiSlice = apiSlice.injectEndpoints({
           url: `${ORDERS_URL}/${id}`
         }),
         keepUnusedDataFor:5
+      }),payOrder: builder.mutation({
+        query: (id,details) => ({
+          url: `${ORDERS_URL}/${id}/pay`,
+          method: 'PUT',
+          body: {...details},
+        })
+      }),getPayPalClientId: builder.query({
+        query: () => ({
+          url: PAYPAL_URL,
+        }),
+        keepUnusedDataFor:5,
       })
     })
   });
