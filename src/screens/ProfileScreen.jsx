0ff0d385161b697760797da4c45995d9a482e4ae
Csaba <csabi627@gmail.com>
2024-01-11 13:33:24 +0100

    feat:create user input form  (ProfileScreen.jsx)

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Row, Col, Form } from "react-bootstrap";
 
 const ProfileScreen = () => {
   const [name, setName] = useState("");
@@ -15,16 +16,66 @@ const ProfileScreen = () => {
     if (userInfo) {
       setName(userInfo.name);
       setEmail(userInfo.email);
-      
     }
   }, [userInfo]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('submitHandler');
-  }
+    console.log("submitHandler");
+  };
 
-  return <div>ProfileScreen</div>;
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="my-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="email" className="my-2">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="password" className="my-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword" className="my-2">
+            <Form.Label>Confrim Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          
+        </Form>
+      </Col>
+      <Col md={9}>Column</Col>
+    </Row>
+  );
 };
 
 export default ProfileScreen;
