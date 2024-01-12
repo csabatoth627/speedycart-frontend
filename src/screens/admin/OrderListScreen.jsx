0f0838067a7faaf34aae2fbb3d2d3d5c32f52416
Csaba <csabi627@gmail.com>
2024-01-12 11:50:26 +0100

    feat:fetch orders from server (OrderListScreen.jsx)

--- src/screens/admin/OrderListScreen.jsx
+++ src/screens/admin/OrderListScreen.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useGetOrdersQuery } from '../../slices/orderApiSlice'
 
 const OrderListScreen = () => {
+  const {data: orders, isLoading, error} = useGetOrdersQuery()
+  
   return (
     <div>OrderListScreen</div>
   )
