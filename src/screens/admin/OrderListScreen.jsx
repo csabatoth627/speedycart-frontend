99fd8161eddce48a07a313a820dc8a66fe9a49f4
Csaba <csabi627@gmail.com>
2024-01-12 11:58:43 +0100

    feat:create conditional rendering to loader, error and table (OrderListScreen.jsx)

--- src/screens/admin/OrderListScreen.jsx
+++ src/screens/admin/OrderListScreen.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { useGetOrdersQuery } from '../../slices/orderApiSlice'
+import React from "react";
+import { useGetOrdersQuery } from "../../slices/orderApiSlice";
+import { Table } from "react-bootstrap";
+import Loading from "../../components/Loading";
+import Message from "../../components/Message";
 
 const OrderListScreen = () => {
-  const {data: orders, isLoading, error} = useGetOrdersQuery()
-  
-  return (
-    <div>OrderListScreen</div>
-  )
-}
+  const { data: orders, isLoading, error } = useGetOrdersQuery();
 
-export default OrderListScreen
+  return (
+    <>
+      <h1>Orders</h1>
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default OrderListScreen;
