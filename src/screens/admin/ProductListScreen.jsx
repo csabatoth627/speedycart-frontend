3d19114fce877b1127d2454a0aeca8b426a8b720
Csaba <csabi627@gmail.com>
2024-01-15 20:44:30 +0100

    feat:create table for the products (ProductListScreen.jsx)

--- src/screens/admin/ProductListScreen.jsx
+++ src/screens/admin/ProductListScreen.jsx
@@ -16,12 +16,44 @@ const ProductListScreen = () => {
           <h1>Products</h1>
         </Col>
         <Col className="text-end">
-            <Button className="btn-sm m-3">
-                <FaEdit/>Create Product
-            </Button>
+          <Button className="btn-sm m-3">
+            <FaEdit />
+            Create Product
+          </Button>
         </Col>
       </Row>
-      {isLoading ? <Loading/> : error ? <Message variant='danger'>{error}</Message> : (<></>)}
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Table striped hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                    <td>{product._id}</td>
+                    <td>{product.name}</td>
+                    <td>${product.price}</td>
+                    <td>{product.category}</td>
+                    <td>{product.brand}</td>
+                    <td></td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
     </>
   );
 };
