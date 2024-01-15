4164b0c4e0317bd67e7093f2b810b5371c3d2c0e
Csaba <csabi627@gmail.com>
2024-01-15 20:26:25 +0100

    feat:fetch products from the server (ProductListScreen.jsx)

--- src/screens/admin/ProductListScreen.jsx
+++ src/screens/admin/ProductListScreen.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useGetProductsQuery } from '../../slices/productApiSlice'
 
 const ProductListScreen = () => {
+    const {data: products, isLoading, error} = useGetProductsQuery();
+    console.log(products);
+
+
   return (
     <div>ProductListScreen</div>
   )
