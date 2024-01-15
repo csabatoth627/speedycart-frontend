e141c40ff327397c2e92a28af75b97ae060a78b8
Csaba <csabi627@gmail.com>
2024-01-15 20:32:50 +0100

    feat:create row for products title and create product button (ProductListScreen.jsx)

--- src/screens/admin/ProductListScreen.jsx
+++ src/screens/admin/ProductListScreen.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import { useGetProductsQuery } from '../../slices/productApiSlice'
+import React from "react";
+import { useGetProductsQuery } from "../../slices/productApiSlice";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { FaEdit } from "react-icons/fa";
+import Loading from "../../components/Loading";
+import Message from "../../components/Message";
+import { LinkContainer } from "react-router-bootstrap";
 
 const ProductListScreen = () => {
-    const {data: products, isLoading, error} = useGetProductsQuery();
-    console.log(products);
-
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
-    <div>ProductListScreen</div>
-  )
-}
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-end">
+            <Button className="btn-sm m-3">
+                <FaEdit/>Create Product
+            </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default ProductListScreen
+export default ProductListScreen;
