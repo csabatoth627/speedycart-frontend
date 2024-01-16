abc0002d278ab84ebd86259817ee38c760c807b4
Csaba <csabi627@gmail.com>
2024-01-16 11:55:08 +0100

    feat:save product details with useState (ProductEditScreen.jsx)

--- src/screens/admin/ProductEditScreen.jsx
+++ src/screens/admin/ProductEditScreen.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useGetProductDetailsQuery } from "../../slices/productApiSlice";
 import { useParams } from "react-router-dom";
 
 const ProductEditScreen = () => {
-const {id: productId} = useParams()
+  const { id: productId } = useParams();
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
@@ -13,10 +13,27 @@ const {id: productId} = useParams()
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
 
-  const {data: product, isLoading, refetch, error} = useGetProductDetailsQuery(productId)
+  const {
+    data: product,
+    isLoading,
+    refetch,
+    error,
+  } = useGetProductDetailsQuery(productId);
 
-  console.log(product);
+  useEffect(() => {
+    if(product){
+        setName(product.name)
+        setPrice(product.price)
+        setImage(product.image)
+        setBrand(product.brand)
+        setCategpry(product.category)
+        setCountInStock(product.countInStock)
+        setDescription(product.description)
+    }
+    
+  }, [product]);
 
+  
   return <div>ProductEditScreen</div>;
 };
 
