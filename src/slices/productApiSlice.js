b0d1ba3082369fb3a94281283081234d02a0f936
Csaba <csabi627@gmail.com>
2024-01-16 14:20:45 +0100

    feat:create uploadProductImage thunk (productApiSlice.js)

--- src/slices/productApiSlice.js
+++ src/slices/productApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from "./apiSlice";
-import { PRODUCTS_URL } from "../constants";
+import { PRODUCTS_URL, UPLOAD_URL } from "../constants";
 
 export const productApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -29,8 +29,15 @@ export const productApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
       invalidatesTags: ['Product']
+    }),
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: UPLOAD_URL,
+        method: 'POST',
+        body: data
+      }),
     })
   }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation } = productApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation } = productApiSlice;
