58f4d7a8e1ebac6bcab3e488378c3a10c13d72d8
Csaba <csabi627@gmail.com>
2024-01-16 10:27:28 +0100

    feat:create fetch post for createProduct and createdProductHandler function with loader (ProductListScreen.jsx)

--- src/screens/admin/ProductListScreen.jsx
+++ src/screens/admin/ProductListScreen.jsx
@@ -1,17 +1,34 @@
 import React from "react";
-import { useGetProductsQuery } from "../../slices/productApiSlice";
+import {
+  useGetProductsQuery,
+  useCreateProductMutation,
+} from "../../slices/productApiSlice";
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import Loading from "../../components/Loading";
 import Message from "../../components/Message";
 import { LinkContainer } from "react-router-bootstrap";
+import { toast } from "react-toastify";
 
 const ProductListScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const [createProduct, { isLoading: loadingCreate }] =
+    useCreateProductMutation();
 
   const deleteHandler = (id) => {
     console.log(id);
-  }
+  };
+
+  const createProductHandler = async () => {
+    if (window.confirm("Are you sure you want to create a new product?")) {
+      try {
+        await createProduct();
+        refetch();
+      } catch (err) {
+        toast.error(err?.data?.message || err.error)
+      }
+    }
+  };
 
   return (
     <>
@@ -20,12 +37,13 @@ const ProductListScreen = () => {
           <h1>Products</h1>
         </Col>
         <Col className="text-end">
-          <Button className="btn-sm m-3">
+          <Button className="btn-sm m-3" onClick={createProductHandler}>
             <FaEdit />
             Create Product
           </Button>
         </Col>
       </Row>
+      {loadingCreate && <Loading/>}
       {isLoading ? (
         <Loading />
       ) : error ? (
@@ -46,21 +64,25 @@ const ProductListScreen = () => {
             <tbody>
               {products.map((product) => (
                 <tr key={product._id}>
-                    <td>{product._id}</td>
-                    <td>{product.name}</td>
-                    <td>${product.price}</td>
-                    <td>{product.category}</td>
-                    <td>{product.brand}</td>
-                    <td>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
                     <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                        <Button variant="light" className="btn-sm mx-2">
-                            <FaEdit/>
-                        </Button>
+                      <Button variant="light" className="btn-sm mx-2" >
+                        <FaEdit />
+                      </Button>
                     </LinkContainer>
-                    <Button variant="danger" className="btn-sm" onClick={() => deleteHandler(product._id)}>
-                    <FaTrash style={{color: "white"}}/>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <FaTrash style={{ color: "white" }} />
                     </Button>
-                    </td>
+                  </td>
                 </tr>
               ))}
             </tbody>
