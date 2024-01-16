5f5f976a8f60f89a7365b98169afaac7c58cdb73
Csaba <csabi627@gmail.com>
2024-01-16 12:42:49 +0100

    feat:create Form (ProductEditScreen.jsx)

--- src/screens/admin/ProductEditScreen.jsx
+++ src/screens/admin/ProductEditScreen.jsx
@@ -7,7 +7,7 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import FormContainer from "../../components/FormContainer";
 import Loading from "../../components/Loading";
 import Message from "../../components/Message";
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 const ProductEditScreen = () => {
   const { id: productId } = useParams();
@@ -16,7 +16,7 @@ const ProductEditScreen = () => {
   const [price, setPrice] = useState(0);
   const [image, setImage] = useState("");
   const [brand, setBrand] = useState("");
-  const [category, setCategpry] = useState("");
+  const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
 
@@ -36,7 +36,7 @@ const ProductEditScreen = () => {
       setPrice(product.price);
       setImage(product.image);
       setBrand(product.brand);
-      setCategpry(product.category);
+      setCategory(product.category);
       setCountInStock(product.countInStock);
       setDescription(product.description);
     }
@@ -49,10 +49,80 @@ const ProductEditScreen = () => {
       </Link>
       <FormContainer>
         <h1>Edit Product</h1>
-        {loadingUpdate && <Loading/>}
-        {isLoading ? <Loading/> : error ? <Message variant="danger">{error}</Message> : (<Form>
-            
-        </Form>)}
+        {loadingUpdate && <Loading />}
+        {isLoading ? (
+          <Loading />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form>
+            <Form.Group controlId="name" className="my-2">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="price" className="my-2">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            {/* IMAGE INPUT PLACEHOLDER */}
+
+            <Form.Group controlId="brand" className="my-2">
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter brand"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="countInStock" className="my-2">
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter coint in stock"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="category" className="my-2">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="description" className="my-2">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Enter description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Button type="submit" variant="primary" className="my-2">
+              Update
+            </Button>
+          </Form>
+        )}
       </FormContainer>
     </>
   );
