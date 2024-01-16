8ffdacb45cb65bf265c5e2dd4aa52afcd1a9b038
Csaba <csabi627@gmail.com>
2024-01-16 11:42:35 +0100

    feat: fetch product from the server (ProductEditScreen.jsx)

--- src/screens/admin/ProductEditScreen.jsx
+++ src/screens/admin/ProductEditScreen.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useGetProductDetailsQuery } from "../../slices/productApiSlice";
+import { useParams } from "react-router-dom";
 
 const ProductEditScreen = () => {
-  return (
-    <div>ProductEditScreen</div>
-  )
-}
+const {id: productId} = useParams()
 
-export default ProductEditScreen
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategpry] = useState("");
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const {data: product, isLoading, refetch, error} = useGetProductDetailsQuery(productId)
+
+  console.log(product);
+
+  return <div>ProductEditScreen</div>;
+};
+
+export default ProductEditScreen;
