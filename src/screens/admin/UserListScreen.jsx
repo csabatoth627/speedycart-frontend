8994f006b58697d11d0ded47272855957251b7c4
Csaba <csabi627@gmail.com>
2024-01-17 20:24:17 +0100

    feat: complete UserListScreen.jsx

--- src/screens/admin/UserListScreen.jsx
+++ src/screens/admin/UserListScreen.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import { useGetUsersQuery } from "../../slices/userApiSlice";
+import { Table, Button } from "react-bootstrap";
+import { FaTimes, FaTrash, FaEdit, FaCheck } from "react-icons/fa";
+import Loading from "../../components/Loading";
+import Message from "../../components/Message";
+import { LinkContainer } from "react-router-bootstrap";
 
 const UserListScreen = () => {
-  return (
-    <div>UserListScreen</div>
-  )
-}
+  const { data: users, refetch, isLoading, error } = useGetUsersQuery();
 
-export default UserListScreen
+  const deleteHandler = (id) => {};
+  return (
+    <>
+      <h1>Users</h1>
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>
+                  <a href={`mailto:${user.email}`}>{user.email}</a>
+                </td>
+
+                <td>
+                  {" "}
+                  {user.isAdmin ? (
+                    <FaCheck style={{ color: "green" }} />
+                  ) : (
+                    <FaTimes style={{ color: "red" }} />
+                  )}
+                </td>
+
+                <td>
+                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                    <Button className="btn-sm" variant="light">
+                      <FaEdit />
+                    </Button>
+                  </LinkContainer>
+                  <Button variant="danger" className="btn-sm" onClick={() => deleteHandler(user._id)}>
+                     <FaTrash style={{color: "white"}}/>
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default UserListScreen;
