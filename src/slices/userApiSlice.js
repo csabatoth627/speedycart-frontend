83a580986ef62275799554bef7fd3f9ad6d3d44f
Csaba <csabi627@gmail.com>
2024-01-17 19:51:40 +0100

    feat: create getUsers thunk (userApiSlice.js)

--- src/slices/userApiSlice.js
+++ src/slices/userApiSlice.js
@@ -2,34 +2,39 @@ import { apiSlice } from "./apiSlice";
 import { USERS_URL } from "../constants";
 
 export const userApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-      login: builder.mutation({
-        query: (data) => ({
-          url: `${USERS_URL}/login`,
-          method: 'POST',
-          body: data,
-        }),
-      }),register: builder.mutation({
-        query: (data) => ({
-          url: USERS_URL,
-          method: 'POST',
-          body: data,
-        }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/login`,
+        method: 'POST',
+        body: data,
       }),
-      logout: builder.mutation({
-        query: () => ({
-          url: `${USERS_URL}/logout`,
-          method: 'POST',
-        }),
+    }), register: builder.mutation({
+      query: (data) => ({
+        url: USERS_URL,
+        method: 'POST',
+        body: data,
       }),
-      profile: builder.mutation({
-        query: (data) => ({
-          url:`${USERS_URL}/profile`,
-          method: 'PUT',
-          body: data,
-        })
-      })
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: 'POST',
+      }),
+    }),
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: 'PUT',
+        body: data,
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL
+      }),
     })
-  });
+  })
+});
 
-  export const {useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation} =  userApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery } = userApiSlice;
