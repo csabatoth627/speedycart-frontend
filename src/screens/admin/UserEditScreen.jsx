b4438c10b2108f1e1bbcee69a0b9fcccc146c25f
Csaba <csabi627@gmail.com>
2024-02-01 15:59:42 +0100

    feat: fetch user data from server (UserEditScreen.jsx)

--- src/screens/admin/UserEditScreen.jsx
+++ src/screens/admin/UserEditScreen.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useGetUserDetailsQuery,  } from '../../slices/userApiSlice'
+import { useParams } from 'react-router-dom'
 
 const UserEditScreen = () => {
+    const {id: userId} = useParams();
+
+    const {
+        data: user,
+        isLoading,
+        refetch,
+        error,
+      } = useGetUserDetailsQuery(userId);
+
+      
+
   return (
     <div>UserEditScreen</div>
   )
